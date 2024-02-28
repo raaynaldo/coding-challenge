84798441265fccb36b703f5f6753989e0c4b8b22
Raynaldo Sutisna <raaynaldo@gmail.com>
2024-02-28 15:50:49 -0500

    feat: implement async function

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,24 @@
-"use strict";
+'use strict';
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
-module.exports = (logSources, printer) => {
+module.exports = async (logSources, printer) => {
+  const { MinPriorityQueue } = require('@datastructures-js/priority-queue');
+  const logQueue = new MinPriorityQueue((log) => log.date);
+
+  const Promises = logSources.map(async (logSource) => {
+    return logQueue.enqueue(await logSource.popAsync());
+  });
+  await Promise.all(Promises); // n log(n)
+
+  while (logQueue.size() > 0) {
+    const logEntry = logQueue.dequeue();
+    printer.print(logEntry);
+  } // n log(n)
+
+  printer.done();
+
   return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
+    resolve(console.log('Async sort complete.'));
   });
 };
