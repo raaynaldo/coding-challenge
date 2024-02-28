70b12896f318bf022668936e8813a26530d04d1a
Raynaldo Sutisna <raaynaldo@gmail.com>
2024-02-28 15:50:41 -0500

    feat: implement sync function

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,22 @@
-"use strict";
+'use strict';
 
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
-  return console.log("Sync sort complete.");
+  const { MinPriorityQueue } = require('@datastructures-js/priority-queue');
+  const logQueue = new MinPriorityQueue((log) => log.date);
+
+  logSources.forEach((logSource) => {
+    const logEntry = logSource.pop();
+    logQueue.enqueue(logEntry);
+  }); // n log(n)
+
+  while (logQueue.size() > 0) {
+    const logEntry = logQueue.dequeue();
+    printer.print(logEntry);
+  } // n log(n)
+
+  printer.done();
+
+  return console.log('Sync sort complete.');
 };
